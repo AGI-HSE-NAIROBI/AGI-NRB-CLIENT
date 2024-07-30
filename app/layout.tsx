e9a3a35a6bd9287fcd00f1a66c16aa8370fbbac0
regan-mu <regansomi@gmail.com>
2024-07-31 01:50:27 +0300

    Next Boilerplate

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "AGI Nairobi Community",
+  description: "We are dedicated to building a thriving AI community focused on responsible and ethical advancements.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      	<body>
+          	{children}
+    	</body>
+    </html>
+  );
+}
